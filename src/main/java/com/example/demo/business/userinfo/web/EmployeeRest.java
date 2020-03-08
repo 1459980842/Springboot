d9364e74d582327320f24b8330176b45bbffbc9d
ding.tao <1459980842@qq.com>
2020-03-08 13:12:40 +0800

    springboot工程

--- src/main/java/com/example/demo/business/userinfo/web/EmployeeRest.java
+++ src/main/java/com/example/demo/business/userinfo/web/EmployeeRest.java
@@ -1,7 +1,6 @@
 package com.example.demo.business.userinfo.web;
 
 
-import com.example.demo.business.userinfo.model.entity.Employee;
 import com.example.demo.business.userinfo.model.form.EmployeeForm;
 import com.example.demo.business.userinfo.model.json.EmployeeJson;
 import com.example.demo.business.userinfo.service.EmployeeService;
