a083ff45494d26d41e44067f3f03d524d698e4c0
ding.tao <1459980842@qq.com>
2020-04-18 10:12:19 +0800

    Default Changelist

---
+++ src/main/java/com/example/demo/business/demo/Demo1.java
@@ -0,0 +1,15 @@
+package com.example.demo.business.demo;
+
+public class Demo1 {
+
+
+    public static void main(String[] args){
+
+        String a = "java,java,hello world";
+        String newA = a.replaceAll("a","");
+
+
+
+    }
+
+}
